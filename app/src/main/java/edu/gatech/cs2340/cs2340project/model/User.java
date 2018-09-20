16816ef65684ae9aa0963fc4654ca35be8adcf96
Jchin88 <johnathan.chin@gmail.com>
2018-09-20 11:34:03 -0400

    Created getters for id and password

--- app/src/main/java/edu/gatech/cs2340/cs2340project/model/User.java
+++ app/src/main/java/edu/gatech/cs2340/cs2340project/model/User.java
@@ -3,4 +3,7 @@ package edu.gatech.cs2340.cs2340project.model;
 public class User {
     private String id = "user";
     private String password = "password";
+
+    public String getID() {return this.id;}
+    public String getPassword() {return this.password;}
 }
