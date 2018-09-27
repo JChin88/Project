a5286381ab416b3d3c15fd55e1057d888796faeb
Hoa V Luu <hluu@gatech.edu>
2018-09-27 13:22:03 -0400

    Update the Register function

--- app/src/main/java/edu/gatech/cs2340/cs2340project/controller/Registration.java
+++ app/src/main/java/edu/gatech/cs2340/cs2340project/controller/Registration.java
@@ -1,7 +1,9 @@
 package edu.gatech.cs2340.cs2340project.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.gatech.cs2340.cs2340project.R;
 
@@ -11,5 +13,22 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void onRegistratePress(View view) {
+
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void onCancelPress(View view) {
+        Intent moveBackWelcome = new Intent(Registration.this, Welcome.class);
+        Registration.this.startActivity(moveBackWelcome);
     }
 }
