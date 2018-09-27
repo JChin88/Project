b9a9a93b60e19c8cd5acd40f1165b13c0bd1cee2
Yumin Jeong <42775579+yjeong73@users.noreply.github.com>
2018-09-27 11:36:16 -0400

    Added LocationEmployee Class

--- app/src/main/java/edu/gatech/cs2340/cs2340project/model/LocationEmployee.java
+++ app/src/main/java/edu/gatech/cs2340/cs2340project/model/LocationEmployee.java
@@ -2,8 +2,8 @@ package edu.gatech.cs2340.cs2340project.model;
 
 public class LocationEmployee extends User {
 
-    public LocationEmployee(String id, String password) {
-        super(id, password);
+    public LocationEmployee(String id, String password, String email, AccountType userType) {
+        super(id, password, email, userType);
     }
 
     public void addInventory() {}
